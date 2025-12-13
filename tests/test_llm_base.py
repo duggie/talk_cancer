61eaa16e099aa1f9e032e89a947493ef3bcb0e0a
Duggie <duggie-git@ninet.co.uk>
2025-12-13 00:59:28 +0000

    chore: fix linting issue

--- tests/test_llm_base.py
+++ tests/test_llm_base.py
@@ -12,5 +12,6 @@ class _ConcreteModel(ChatModel):
 
 @pytest.mark.asyncio
 async def test_chatmodel_chat_raises_not_implemented() -> None:
+    """test NotImplementedError"""
     with pytest.raises(NotImplementedError):
         await _ConcreteModel().chat([ChatMessage(role="user", content="hi")])
